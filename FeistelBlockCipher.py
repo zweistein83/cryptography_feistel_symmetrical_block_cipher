a26aa046958fb83637abcb6c45ff16ea5ff89a10
Zweistein83 <milo83@gmail.com>
2020-02-21 21:55:22 +0100

    readme and some small changes

--- FeistelBlockCipher.py
+++ FeistelBlockCipher.py
@@ -5,142 +5,143 @@ import hashlib
 
 class FeistelBlockCipher:
 
-	def __init__(self, padding_char = "@", text_encoding = "utf-8"):
-		self.padding_char = padding_char
-		self.text_encoding = text_encoding
+    def __init__(self, padding_char="@", text_encoding="utf-8"):
+        self.padding_char = padding_char
+        self.text_encoding = text_encoding
 
-	def pad_odd(self, txt):
-		"""
-		Adds 1 character right padding to input if it's length is odd.
-		"""
-		if len(txt) % 2 != 0:
-			txt += self.padding_char
-		return txt
+    def __pad_odd(self, txt):
+        """
+        Adds 1 character right padding to input if it's length is odd.
+        """
+        if len(txt) % 2 != 0:
+            txt += self.padding_char
+        return txt
 
-	def remove_pad(self, txt):
-		"""
-		Removes padding.
-		"""
-		if txt[-1] == self.padding_char:
-			return txt[:-1]
-		return txt
+    def __remove_pad(self, txt):
+        """
+        Removes padding.
+        """
+        if txt[-1] == self.padding_char:
+            return txt[:-1]
+        return txt
 
-	def to_bytes(self, txt):
-		"""
-		Converts from string to bytes.
-		"""
-		if type(txt) is bytes:
-			return txt
-		return bytes(txt, self.text_encoding)
+    def __to_bytes(self, txt):
+        """
+        Converts from string to bytes.
+        """
+        if type(txt) is bytes:
+            return txt
+        return bytes(txt, self.text_encoding)
 
-	def rolling_xor(self, in1, in2):
-		"""
-		Xor bytewise in1 against in2.
-		"""
-		# in2 must be the same length as in1 or parts of the message would not get encrypted/decrypted
-		while len(in2) < len(in1):
-			in2 = in2 * 2
-		in2 = in2[:len(in1)]
+    def __rolling_xor(self, in1, in2):
+        """
+        Xor bytewise in1 against in2.
+        """
+        # in2 must be the same length as in1 or parts of the message would not get encrypted/decrypted
+        while len(in2) < len(in1):
+            in2 = in2 * 2
+        in2 = in2[:len(in1)]
 
-		return bytes([a ^ b for a,b in zip(in1, in2)])
+        return bytes([a ^ b for a, b in zip(in1, in2)])
 
-	def salted_md5(self, txt, salt):
-		"""
-		Returns salted md5 as bytes
-		Inputs:
-			txt: message as string.
-			salt: key/salt as string.
-		"""
-		md5_encoded = hashlib.md5(txt + self.to_bytes(salt))
-		return md5_encoded.digest()
+    def __salted_md5(self, txt, salt):
+        """
+        Returns salted md5 as bytes
+        Inputs:
+            txt: message as string.
+            salt: key/salt as string.
+        """
+        md5_encoded = hashlib.md5(txt + self.__to_bytes(salt))
 
-	def encode_message(self, message, encryption_keys, crypto_func=None):
-		"""
-			Encodes/encrypts message.
-			Inputs:
-				message: 		The message that is going to be encrypted (string).
+        return md5_encoded.digest()
 
-				encryption_keys: Different keys as strings. Use 2 or more different keys/salts.
+    def __feistel_symmetrical_block_cipher(self, message, keys, crypto_func):
+        """
+            Inputs:
+                message: 		The message that is going to be encrypted or decrypted
 
-				crypto_func: 	function that encrypts input in form (message, key/salt).
-								- The crypto function must return bytes.
+                keys: 			Different keys as strings. Use 2 or more different keys/salts.
 
-			Returns:
-				Ciphertext as hexadecimal string.
-		"""
-		if crypto_func is None:
-			crypto_func = self.salted_md5
+                crypto_func: 	function that encrypts input in form (message, key/salt).
+                                - The crypto function must return bytes.
 
-		padded_message = self.pad_odd(message)
-		message_bytes = self.to_bytes(padded_message)
-		encoded_message = self.feistel_symmetrical_block_cipher(message_bytes, encryption_keys, crypto_func)
-		return encoded_message.hex()
+            Returns:
+                Encrypted or decrypted bytes
+        """
+        second_half_index = (len(message) // 2)
 
-	def decode_message(self, encoded_message, encryption_keys, crypto_func=None):
-		"""
-			Inputs:
-				Decodes/decrypts encoded message. The encryption keys must be the same as used to encrypt the message.
+        n_left = message[:second_half_index]
+        n_right = message[second_half_index:]
+        for key in keys:
+            n_right_tmp = n_right
+            n_right = self.__rolling_xor(n_left, crypto_func(n_right_tmp, key))
+            n_left = n_right_tmp
 
-				message: 		The message that is going to be decrypted (hex string).
+        return n_right + n_left
 
-				encryption_keys:Different keys as strings. Use 2 or more different keys/salts (list of strings).
+    def encode_message(self, message, encryption_keys, crypto_func=None):
+        """
+            Encodes/encrypts message.
+            Inputs:
+                message: 		The message that is going to be encrypted (string).
 
-				crypto_func: 	function that encrypts input in form (message, key/salt) (function).
-								- The crypto function must return bytes.
+                encryption_keys: Different keys as strings. Use 2 or more different keys/salts.
 
-			Returns:
-				Decoded message string
-		"""
-		if crypto_func is None:
-			crypto_func = self.salted_md5
+                crypto_func: 	function that encrypts input in form (message, key/salt).
+                                - The crypto function must return bytes.
 
-		bytes_encoded = bytes.fromhex(encoded_message)
-		decoded_message = self.feistel_symmetrical_block_cipher(bytes_encoded, encryption_keys[::-1], crypto_func)
+            Returns:
+                Ciphertext as hexadecimal string.
+        """
+        if crypto_func is None:
+            crypto_func = self.__salted_md5
 
-		try:
-			decoded_message = decoded_message.decode(self.text_encoding)
-		except UnicodeDecodeError:
-			raise KeyError("Unable to decrypt message!")
+        padded_message = self.__pad_odd(message)
+        message_bytes = self.__to_bytes(padded_message)
+        encoded_message = self.__feistel_symmetrical_block_cipher(message_bytes, encryption_keys, crypto_func)
+        return encoded_message.hex()
 
-		return self.remove_pad(decoded_message)
+    def decode_message(self, encoded_message, encryption_keys, crypto_func=None):
+        """
+            Inputs:
+                Decodes/decrypts encoded message. The encryption keys must be the same as used to encrypt the message.
 
-	def feistel_symmetrical_block_cipher(self, message, keys, crypto_func):
-		"""
-			Inputs:
-				message: 		The message that is going to be encrypted or decrypted
+                message: 		The message that is going to be decrypted (hex string).
 
-				keys: 			Different keys as strings. Use 2 or more different keys/salts.
+                encryption_keys:Different keys as strings. Use 2 or more different keys/salts (list of strings).
 
-				crypto_func: 	function that encrypts input in form (message, key/salt).
-								- The crypto function must return bytes.
+                crypto_func: 	function that encrypts input in form (message, key/salt) (function).
+                                - The crypto function must return bytes.
 
-			Returns:
-				Encrypted or decrypted bytes
-		"""
-		second_half_index = (len(message) // 2 )
+            Returns:
+                Decoded message string
+        """
+        if crypto_func is None:
+            crypto_func = self.__salted_md5
 
-		n_left = message[:second_half_index]
-		n_right = message[second_half_index:]
-		for key in keys:
-			n_right_tmp = n_right
-			n_right = self.rolling_xor(n_left, crypto_func(n_right_tmp, key))
-			n_left = n_right_tmp
+        bytes_encoded = bytes.fromhex(encoded_message)
+        decoded_message = self.__feistel_symmetrical_block_cipher(bytes_encoded, encryption_keys[::-1], crypto_func)
 
-		return n_right + n_left
+        try:
+            decoded_message = decoded_message.decode(self.text_encoding)
+        except UnicodeDecodeError:
+            raise KeyError("Unable to decrypt message!")
+
+        return self.__remove_pad(decoded_message)
 
 
 if __name__ == "__main__":
+    message = "this is a test to check if the decoded message is the same as the original message after encoding and decryption"
+    keys = ["salt", "pepper", "oregano", "cinnamon", "any text at all", "0113423434øå5634636basdaeer%tertasd_.,Æ"]
 
-	message = "this is a test to check if the decoded message is the same as the original message after encoding and decryption"
-	keys = ["salt", "pepper", "oregano", "cinnamon", "any text at all", "0113423434øå5634636basdaeer%tertasd_.,Æ"]
+    crypto = FeistelBlockCipher()
 
-	crypto = FeistelBlockCipher()
-	encoded = crypto.encode_message(message, keys)
-	decoded = crypto.decode_message(encoded, keys)
+    encoded = crypto.encode_message(message, keys)
+    print("Original message:")
+    print(message)
+    print("\nEncoded message:")
+    print(encoded)
 
-	print("Original message:")
-	print(message)
-	print("\nEncoded message:")
-	print(encoded)
-	print("\nDecoded message:")
-	print(decoded)
+    decoded = crypto.decode_message(encoded, keys)
+    print("\nDecoded message:")
+    print(decoded)
---
+++ readme.md
@@ -0,0 +1,55 @@
+## Feistel symmetrical block cipher
+[https://en.wikipedia.org/wiki/Feistel_cipher](https://en.wikipedia.org/wiki/Feistel_cipher)
+ 
+crypto = FeistelBlockCipher(padding_char="@", text_encoding="utf-8")
+
+ - **padding_char** (optional): one character which is used for padding if the message is of odd length
+ - **text_encoding** (optional): used for converting from and to bytes. Default is "utf-8".
+
+**Encode message:**
+  
+encoded_message = crypto.encode_message(message, keys, crypto_func) 
+
+- **message:** message that should be encoded as a string.
+- **keys:** keys used for encryption as a list of strings. Each key should be unique.
+- - **crypto_func** (optional): You can pass any cryptographical function that returns bytes. Must also take 2 inputs (message, key). Default is salted md5 which uses key as salt.
+
+**Decode message:**
+ 
+decoded_message = crypto.decode_message(encoded, keys, crypto_func)
+- **message:** message that should be encoded as a string.
+- **keys:** keys used for decryption as a list of strings. This must be identical to what was used to encrypt the message.
+- **crypto_func** (optional): You can pass any cryptographical function that returns bytes. Must also take 2 inputs (message, key). Default is salted md5 which uses key as salt.  Requires the same encryption algorithm as used to encrypt the message.
+
+**Example:**
+	
+Encode message
+
+    from FeistelBlockCipher import FeistelBlockCipher
+    
+    message="Lorem ipsum dolor sit amet."  
+    keys = ["secret_key1", "secret_key2"]  
+    
+    crypto = FeistelBlockCipher()  
+     
+    encoded = crypto.encode_message(message, keys)  
+    
+    print("\nEncoded message:")  
+    print(encoded)  
+    -->Encoded message:
+	-->8392cefb63d7a06763ac72c1724d29ed32d4838a216a33551468cfb4
+
+    
+Decode message:
+
+    from FeistelBlockCipher import FeistelBlockCipher"
+    
+    crypto = FeistelBlockCipher() 
+     
+    keys = ["secret_key1", "secret_key2"] 
+    encoded_message = "8392cefb63d7a06763ac72c1724d29ed32d4838a216a33551468cfb4"
+    decoded = crypto.decode_message(encoded_message, keys)  
+    print("\nDecoded message:")  
+    print(decoded)
+    -->Decoded message:
+    -->Lorem ipsum dolor sit amet.
